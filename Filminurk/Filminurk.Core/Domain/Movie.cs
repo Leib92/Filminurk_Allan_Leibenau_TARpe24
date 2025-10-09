2b6e6aaf62c0a5e6890c62231bc46d1c0841af6a
Leib92 <opilane@HP-CZC321705Y>
2025-10-09 11:05:36 +0300

    Movies modeldata for both domain and viewmodel

---
+++ Filminurk/Filminurk.Core/Domain/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public string Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public decimal? CurrentRating { get; set; }
+        // public List<UserComment>? Reviews { get; set; }
+
+        /* 3 enda valitud */
+
+        public string? Genre { get; set; }
+        public int? Budget { get; set; }
+        public int? BoxOffice { get; set; }
+
+    }
+}
---
+++ Filminurk/Filminurk/Models/Models/MoviesIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Filminurk.Models.Models
+{
+    public class MoviesIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public decimal? CurrentRating { get; set; }
+        // public List<UserComment>? Reviews { get; set; }
+
+        /* 2 enda valitud */
+
+        public string? Genre { get; set; }
+    }
+}
