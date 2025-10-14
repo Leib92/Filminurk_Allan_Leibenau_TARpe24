9071f8c4d8a28d4ca59dc121ba253fdbe52e0cb0
Leib92 <opilane@HP-CZC321705Q>
2025-10-14 14:13:23 +0300

    Working on Deletion - Added MoviesDeleteViewModel - New Delete view added for Movies - Index now can access delete

---
+++ Filminurk/Filminurk/Models/Movies/MoviesDeleteViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Filminurk.Models.Movies
+{
+    public class MoviesDeleteViewModel
+    {
+        public Guid? ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? FirstPublished { get; set; }
+        public string? Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public double? CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+
+        /* 3 enda valitud andmet */
+        public string? Genre { get; set; }
+        public int? Budget { get; set; }
+        public int? BoxOffice { get; set; }
+
+        /* andmebaasi jaoks vajalikud */
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
