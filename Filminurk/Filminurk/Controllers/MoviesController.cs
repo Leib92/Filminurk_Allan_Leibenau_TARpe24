5aade73c109eb446d5ee2720b1aa6fa7d011c2ab
Leib92 <opilane@HP-CZC321705Q>
2025-10-16 11:52:12 +0300

    added delete methods in interface, service, and controller

--- Filminurk/Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Filminurk/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using Filminurk.Core.ServiceInterface;
 using Filminurk.Data;
 using Filminurk.Models.Movies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Filminurk.Controllers
 {
@@ -43,26 +44,66 @@ namespace Filminurk.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(MoviesCreateViewModel vm)
         {
-            var dto = new MoviesDTO()
+            if (ModelState.IsValid)
             {
-                ID = vm.ID,
-                Title = vm.Title,
-                Description = vm.Description,
-                FirstPublished = vm.FirstPublished,
-                Director = vm.Director,
-                Actors = vm.Actors,
-                CurrentRating = vm.CurrentRating,
-                Genre = vm.Genre,
-                Budget = vm.Budget,
-                BoxOffice = vm.BoxOffice,
-                EntryCreatedAt = vm.EntryCreatedAt,
-                EntryModifiedAt = vm.EntryModifiedAt
+                var dto = new MoviesDTO()
+                {
+                    ID = vm.ID,
+                    Title = vm.Title,
+                    Description = vm.Description,
+                    FirstPublished = vm.FirstPublished,
+                    Director = vm.Director,
+                    Actors = vm.Actors,
+                    CurrentRating = vm.CurrentRating,
+                    Genre = vm.Genre,
+                    Budget = vm.Budget,
+                    BoxOffice = vm.BoxOffice,
+                    EntryCreatedAt = vm.EntryCreatedAt,
+                    EntryModifiedAt = vm.EntryModifiedAt
 
-            };
-            var result = await _movieServices.Create(dto);
-            if (result == null) 
-            {
+                };
+                var result = await _movieServices.Create(dto);
+                if (result == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
                 return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var movie = await _movieServices.DetailsAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            var vm = new MoviesDeleteViewModel();
+            vm.ID = id;
+            vm.Title = movie.Title;
+            vm.Description = movie.Description;
+            vm.FirstPublished = movie.FirstPublished;
+            vm.Director = movie.Director;
+            vm.Actors = movie.Actors;
+            vm.CurrentRating = movie.CurrentRating;
+            vm.Genre = movie.Genre;
+            vm.Budget = movie.Budget;
+            vm.BoxOffice = movie.BoxOffice;
+            vm.EntryCreatedAt = movie.EntryCreatedAt;
+            vm.EntryModifiedAt = movie.EntryModifiedAt;
+            
+            return View(vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmation(Guid id)
+        {
+            var movie = await _movieServices.Delete(id);
+            if (movie == null) 
+            {
+                return NotFound();
             }
             return RedirectToAction(nameof(Index));
         }
